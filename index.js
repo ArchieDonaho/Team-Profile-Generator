a1436c5cde77bac0140ce9fe90b429b62830c474
AD <archied5150@yahoo.com>
2022-04-06 22:27:36 -0500

    fixed variable issue

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 const Team = require('./lib/Team.js');
-// const inquirer = require('inquirer');
-// const Manager = require('./lib/Manager.js');
-// const Engineer = require('./lib/Engineer.js');
-// const Intern = require('./lib/Intern.js');
-
 
 const team = new Team().start();
 
---
+++ src/GenerateHTML.js
@@ -0,0 +1,9 @@
+
+
+generateHTML = (manager, engineers, interns) => {
+    console.table(manager);
+    console.table(engineers);
+    console.table(interns);
+}
+
+module.exports = generateHTML;
